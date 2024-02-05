f02e583c906788205bea30119f9b4dcc482ac7e1
Alex Claes <hello@aclaes.com>
2024-02-05 21:32:30 +0100

    fix for relative paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/_assets/normalize.css');
   eleventyConfig.addPassthroughCopy('src/_assets/styles.css');
@@ -7,5 +9,18 @@ module.exports = function (eleventyConfig) {
     return collectionApi
       .getFilteredByGlob('src/uebungen/*.md')
       .sort((a, b) => a.inputPath.localeCompare(b.inputPath));
+  });
+
+  eleventyConfig.addFilter('relativeUrl', (url, page) => {
+    if (!url.startsWith('/')) {
+      throw new Error('URL is already relative');
+    }
+    const relativeUrl = path.relative(page.filePathStem, url);
+
+    if (page.filePathStem === '/index') {
+      return path.relative('..', relativeUrl);
+    } else {
+      return relativeUrl;
+    }
   });
 };
